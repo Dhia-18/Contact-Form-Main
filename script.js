28a3f392414d85c55034d5278249fe0597a67bab
Dhia <77319209+Dhia-18@users.noreply.github.com>
2024-08-12 21:46:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,70 +1,113 @@
-const firstNameInput = document.getElementById("first-name-input");
-const lastNameInput = document.getElementById("last-name-input");
-const emailInput = document.getElementById("email-input");
+const form = document.querySelector("form");
+
+const firstName = document.getElementById("first-name-input");
+const lastName = document.getElementById("last-name-input");
+const email = document.getElementById("email-input");
 const radioButtons = document.querySelectorAll("input[type=radio]");
-const messageInput = document.getElementById("message-input");
-const checkboxInput = document.getElementById("checkbox-input");
+const message = document.getElementById("message-input");
+const checkbox = document.getElementById("checkbox-input");
 
 const successContainer = document.querySelector(".success-notification-container");
-
-
-const submitButton = document.querySelector("button");
-
-function validEmail(){
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(emailInput.value);
-}
-
-function noRadioButtonsChecked() {
-    return !Array.from(radioButtons).some(radio => radio.checked);
-}
+const radioButtonsContainers = document.querySelectorAll(".type-container div");
 
 function showError(input,msg){
     const errorMessage = input.parentElement.querySelector(".error-message");
 
+    input.style.borderColor="var(--red)";
     errorMessage.textContent=`${msg}`;
 }
 
-submitButton.addEventListener("click",(e)=>{
+function emailValid(){
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email.value);
+}
+
+function buttonsChecked(){
+    return(radioButtons[0].checked || radioButtons[1].checked);
+}
+
+/* Allow users to click on any position of the radioButton div */
+
+radioButtonsContainers.forEach(container=>{
+    container.addEventListener("click",()=>{
+        /* Reset background color of all containers */
+        radioButtonsContainers.forEach(container => {
+            container.style.backgroundColor = "";
+            container.style.borderColor = "";
+        });
+
+        container.querySelector("input[type=radio]").checked=true;
+
+        container.style.backgroundColor="var(--green-200)";
+        container.style.borderColor="var(--green-600)";
+    });
+});
+
+/* Allow users to click on the label in order to check the checkbox */
+
+checkbox.parentElement.querySelector("label").addEventListener("click",()=>{
+    checkbox.checked ? checkbox.checked=false : checkbox.checked=true;
+});
+
+/* Reset Form */
+
+function resetErrors(){
+    const errorMessages = form.querySelectorAll(".error-message");
+    const inputs = form.querySelectorAll("input, textarea");
+
+    errorMessages.forEach(error=>{
+        error.textContent="";
+    });
+
+    inputs.forEach(input => {
+        input.style.borderColor = "";
+    });
+}
+
+form.addEventListener("submit",(e)=>{
     e.preventDefault();
+    resetErrors();
 
-    let bool = true;
+    let isValid = true;
 
-    if(firstNameInput.value===""){
-        showError(firstNameInput,"This field is required");
-        bool=false;
-    }
-    
-    if(lastNameInput.value===""){
-        showError(lastNameInput,"This field is required");
-        bool=false;
+    if(firstName.value===""){
+        showError(firstName,"This field is required");
+        isValid = false;
     }
 
-    if(emailInput.input===""){
-        showError(emailInput,"This field is required");
-        bool=false;
-    }
-    else if(!validEmail()){
-        showError(emailInput,"Please enter a valid email address");
-        bool=false;
+    if(lastName.value===""){
+        showError(lastName,"This field is required");
+        isValid = false;
     }
 
-    if(noRadioButtonsChecked()){
-        showError(radioButtons[0].parentElement.parentElement,"Please select a query type");
-        bool=false;
+    if(email.value===""){
+        showError(email,"This field is required");
+        isValid = false;
+    }
+    else if(!emailValid()){
+        showError(email,"Please enter a valid email address");
+        isValid = false;
     }
 
-    if(messageInput.value===""){
-        showError(messageInput,"This field is required");
-        bool=false;
+    if(!buttonsChecked()){
+        const errorMessage = radioButtons[0].closest(".type-container").querySelector(".error-message");
+        errorMessage.textContent = "Please select a query type";
+        isValid = false;
     }
 
-    if(!checkboxInput.checked){
-        showError(checkboxInput,"To submit this form, please consent to being contacted");
-        bool=false;
+    if(message.value===""){
+        showError(message,"This field is required");
+        isValid = false;
     }
 
-    if(bool){
+    if(!checkbox.checked){
+        const errorMessage = checkbox.parentElement.querySelector(".error-message");
+        errorMessage.textContent="To submit this form, please conset to being contacted";
+        isValid = false;
+    }
+
+    if(isValid){
         successContainer.classList.remove("hidden");
     }
-})
+
+});
